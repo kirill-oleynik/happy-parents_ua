b18c533f60a3356ce1134a8957206a779fc3b8c5
Kirill Oleynik <kirill.olejnik@gmail.com>
2022-07-24 19:40:14 +0300

    Drop User entity

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'pages#home'
