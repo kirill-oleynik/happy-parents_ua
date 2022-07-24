667e0fb2db801681c1b97d34879caefd3ee20fa2
Kirill Oleynik <kirill.olejnik@gmail.com>
2022-07-24 18:23:03 +0300

    Add and setup gems for test & dev envs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'pry'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
@@ -17,4 +18,10 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
