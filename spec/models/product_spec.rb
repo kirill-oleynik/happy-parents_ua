4e879d5e6696ac37f6e39bae3b6c981169ca48d5
Kirill Oleynik <kirill.olejnik@gmail.com>
2022-08-02 10:28:41 +0300

    Add product entity with vasic attributes

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+RSpec.describe Product, type: :model do
+  subject(:product) { build(:product) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:code) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_uniqueness_of(:code) }
+  end
+end
