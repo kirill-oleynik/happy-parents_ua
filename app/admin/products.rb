33e9f1e3fb0e0440ca51a0519ba70a3597392f74
Kirill Oleynik <kirill.olejnik@gmail.com>
2022-10-05 18:32:11 +0300

    Rewise locales structure

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -19,72 +19,72 @@ ActiveAdmin.register Product do
   # ================== index page =======================
   index do
     selectable_column
-    column t('active_admin.store.products.index.table.column_labels.code'), :code
-    column t('active_admin.store.products.index.table.column_labels.is_published'), :is_published
-    column t('active_admin.store.products.index.table.column_labels.title'), :title
-    column t('active_admin.store.products.index.table.column_labels.price'), :price
+    column t('active_admin.store.products.labels.code'), :code
+    column t('active_admin.store.products.labels.is_published'), :is_published
+    column t('active_admin.store.products.labels.title'), :title
+    column t('active_admin.store.products.labels.price'), :price
     actions default: true do |product|
-      link_to t('active_admin.store.products.index.actions.show_in_app'), product_path(product.slug), target: '_blank',
+      link_to t('active_admin.actions.show_in_app'), product_path(product.slug), target: '_blank',
                                                                                                       rel: 'noopener'
     end
   end
   # ================== show page =======================
   show do
     attributes_table do
-      row(t('active_admin.store.products.show.labels.code'), &:code)
-      row(t('active_admin.store.products.show.labels.title'), &:title)
-      row(t('active_admin.store.products.show.labels.is_published'), &:is_published)
-      row(t('active_admin.store.products.show.labels.price'), &:price)
-      row(t('active_admin.store.products.form.labels.slug'), &:slug)
-      row(t('active_admin.store.products.show.labels.male_gender'), &:male_gender)
-      row(t('active_admin.store.products.show.labels.female_gender'), &:female_gender)
-      row(t('active_admin.store.products.show.labels.min_age'), &:min_age)
-      row(t('active_admin.store.products.show.labels.max_age'), &:max_age)
+      row(t('active_admin.store.products.labels.code'), &:code)
+      row(t('active_admin.store.products.labels.title'), &:title)
+      row(t('active_admin.store.products.labels.is_published'), &:is_published)
+      row(t('active_admin.store.products.labels.price'), &:price)
+      row(t('active_admin.store.products.labels.slug'), &:slug)
+      row(t('active_admin.store.products.labels.male_gender'), &:male_gender)
+      row(t('active_admin.store.products.labels.female_gender'), &:female_gender)
+      row(t('active_admin.store.products.labels.min_age'), &:min_age)
+      row(t('active_admin.store.products.labels.max_age'), &:max_age)
     end
     active_admin_comments
   end
   # end
   # ================== filters =======================
   filter :code,
-         label: I18n.t('active_admin.store.products.filters.labels.code')
+         label: I18n.t('active_admin.store.products.labels.code')
   filter :slug
   filter :is_published,
-         label: I18n.t('active_admin.store.products.filters.labels.is_published')
+         label: I18n.t('active_admin.store.products.labels.is_published')
   filter :title,
-         label: I18n.t('active_admin.store.products.filters.labels.title')
+         label: I18n.t('active_admin.store.products.labels.title')
   filter :male_gender,
-         label: I18n.t('active_admin.store.products.filters.labels.male_gender')
+         label: I18n.t('active_admin.store.products.labels.male_gender')
   filter :female_gender,
-         label: I18n.t('active_admin.store.products.filters.labels.female_gender')
+         label: I18n.t('active_admin.store.products.labels.female_gender')
   filter :min_age,
-         label: I18n.t('active_admin.store.products.filters.labels.min_age')
+         label: I18n.t('active_admin.store.products.labels.min_age')
   filter :max_age,
-         label: I18n.t('active_admin.store.products.filters.labels.max_age')
+         label: I18n.t('active_admin.store.products.labels.max_age')
   filter :price_cents,
-         label: I18n.t('active_admin.store.products.filters.labels.price_cents')
+         label: I18n.t('active_admin.store.products.labels.price_cents')
   # ================== forms =======================
   form do |form|
     form.inputs do
       form.input :code,
-                 label: t('active_admin.store.products.form.labels.code')
+                 label: t('active_admin.store.products.labels.code')
       form.input :is_published,
-                 label: t('active_admin.store.products.form.labels.is_published')
+                 label: t('active_admin.store.products.labels.is_published')
       form.input :title,
-                 label: t('active_admin.store.products.form.labels.title')
+                 label: t('active_admin.store.products.labels.title')
       form.input :title_ru,
-                 label: t('active_admin.store.products.form.labels.title_ru')
+                 label: t('active_admin.store.products.labels.title_ru')
       form.input :slug,
-                 label: t('active_admin.store.products.form.labels.slug')
+                 label: t('active_admin.store.products.labels.slug')
       form.input :male_gender,
-                 label: t('active_admin.store.products.form.labels.male_gender')
+                 label: t('active_admin.store.products.labels.male_gender')
       form.input :female_gender,
-                 label: t('active_admin.store.products.form.labels.female_gender')
+                 label: t('active_admin.store.products.labels.female_gender')
       form.input :min_age,
-                 label: t('active_admin.store.products.form.labels.min_age')
+                 label: t('active_admin.store.products.labels.min_age')
       form.input :max_age,
-                 label: t('active_admin.store.products.form.labels.max_age')
+                 label: t('active_admin.store.products.labels.max_age')
       form.input :price,
-                 label: t('active_admin.store.products.form.labels.price')
+                 label: t('active_admin.store.products.labels.price_form')
       form.semantic_errors
       form.actions
     end
