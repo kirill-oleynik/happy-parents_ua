19c761edfff3d86fe1c7289c663a773c2ccc8527
Kyryl Ωliinyk <kirill.olejnik@gmail.com>
2023-06-08 20:44:01 +0300

    Add Manufacturer entity

---
+++ spec/models/manufacturer_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.describe Manufacturer do
+  subject(:supplier) { build(:manufacturer) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
+end
